48a26b7e0aa7527cb45f6a28c2adf2d7ec832095
maxicassarino <maximocassarino@gmail.com>
2025-07-17 19:43:32 -0300

    Excepcion Personalizada y Roles

--- backend/src/main/java/backend/backend/Marcas/exception/MarcaNoEncontradaException.java
+++ backend/src/main/java/backend/backend/Marcas/exception/MarcaNoEncontradaException.java
@@ -2,6 +2,6 @@ package backend.backend.Marcas.exception;
 
 public class MarcaNoEncontradaException extends RuntimeException {
     public MarcaNoEncontradaException(Long id) {
-        super("Marca con ID " + id + " no encontrada");
+        super("Marca con ID " + id + " no encontrada TEST");
     }
 }
